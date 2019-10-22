92fb8d0bf47747b3a9a4181a12226102a6f80281
User <User@YD03-20>
2019-10-22 17:51:10 +0900

    20191022_1750

--- JavaScript/src/emp/EmpServerlet.java
+++ JavaScript/src/emp/EmpServerlet.java
@@ -10,29 +10,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/EmpServ") // 이 페이지의 주소
-public class EmpServ extends HttpServlet {
+import net.sf.json.JSONArray;
+
+@WebServlet("/EmpServerlet") // 주소
+public class EmpServerlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public EmpServ() {
+	public EmpServerlet() {
+		super();
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 		PrintWriter out = response.getWriter();
 		EmpDAO dao = new EmpDAO();
 		List<Employee> list = dao.getEmpList();
-		out.println("{\"data\":[");
-		int cnt = 0;
-		for (Employee emp : list) {
-			out.println("{\"firstName\":\"" + emp.getFirstName() + "\",\"lastName\":\"" + emp.getLastName()
-					+ "\",\"email\":\"" + emp.getEmail() + "\"}");// 스크립트 출력(\"는 글자 그대로 보이게)
-			cnt++;
-			if (list.size() != cnt)
-				out.println(",");
-		}
-		out.println("]}");
+		out.println(JSONArray.fromObject(list));
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
